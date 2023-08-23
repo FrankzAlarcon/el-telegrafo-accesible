cdb537522057517358ef6caaa65745830cb3c41d
marioVillamar <mjvc98@hotmail.es>
2023-08-23 11:16:45 -0500

    feat: Cambios en layout dentro de la etiqueta nav

---
+++ src/app/deportes/page.tsx
@@ -0,0 +1,9 @@
+import React, { type FC } from 'react'
+
+const SportsNewsPage: FC<any> = () => {
+  return (
+    <div>SportsNewsPage</div>
+  )
+}
+
+export default SportsNewsPage
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,16 @@ export default function RootLayout ({
   return (
     <html lang="es">
       <body className={`${oswald.className} bg-white min-h-screen`}>
-        <nav className='w-full bg-primary h-[50px] flex '>
+        <nav className='w-full flex flex-col'>
+          <div className='py-2 p-5 flex flex-row justify-between items-center' >
+            <img src="/assets/imgs/logo.png" alt="El Telégrafo" />
+            <div className='flex gap-14'>
+              {/* Colocar iconos de busqueda y componete de modo oscuro */}
+              <img src="/assets/imgs/ico-buscar.png" alt="El Telégrafo" />
+              <img src="/assets/imgs/ico-usuario.png" alt="El Telégrafo" />
+            </div>
+          </div>
+          <div className=' bg-primary  px-[88px] w-full h-[50px] flex '>
           {
             routes.map((route) => (
               <NavLink
@@ -31,8 +40,12 @@ export default function RootLayout ({
               </NavLink>
             ))
           }
+          </div>
         </nav>
-        {children}
+        <div>
+          {children}
+        </div>
+        <footer></footer>
       </body>
     </html>
   )
